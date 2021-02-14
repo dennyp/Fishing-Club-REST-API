4c850d16c5f2df8641d59a19e995a83699e6e6ab
Denny Petersson <petersson.denny@gmail.com>
2021-02-14 14:12:42 +0100

    adding mongoose to connect to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 'use strict'
 
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// Connection string to MongoDb
+const CONNECTION_STRING = `${process.env.DB_URL}`
+
+// Connect to database
+mongoose.connect(
+  CONNECTION_STRING,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+)
+
+// Mongoose connection
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log(`Connected to database ${process.env.DB_NAME}!`)
+})
 
 app.use(express.json())
 
