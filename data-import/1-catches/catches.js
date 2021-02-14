1fc012d35486a39ae13bda1ca466b1796f2f629e
Denny Petersson <petersson.denny@gmail.com>
2021-02-14 20:02:52 +0100

    adding seeding for database

---
+++ data-import/1-catches/catches.js
@@ -0,0 +1,19 @@
+const { getObjectId } = require('mongo-seeding')
+
+const names = ['Nicklas', 'Jakob', 'Lovisa', 'Erik', 'Lotta', 'Isak', 'Lina', 'Linda', 'Amanda', 'Karl']
+const min = 18
+const max = 100
+
+module.exports = names.map(name => ({
+  username: name,
+  longitud: Math.floor(Math.random() * (max - min + 1)) + min,
+  latitud: Math.floor(Math.random() * (max - min + 1)) + min,
+  locationWater: 'Vättern',
+  locationCity: 'Jönköping',
+  species: 'Abborre',
+  weight: 1.9,
+  length: 50,
+  imageUrl: `http://image.com/${name}`,
+  timestamp: Date.now,
+  _id: getObjectId(name)
+}))
---
+++ seed.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const path = require('path')
+const { Seeder } = require('mongo-seeding')
+require('dotenv').config()
+
+const config = {
+  database: process.env.DB_URL,
+  dropDatabase: true
+}
+
+const seeder = new Seeder(config)
+
+const collections = seeder.readCollectionsFromPath(
+  path.resolve('./data-import'),
+  {
+    transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId]
+  }
+)
+
+seeder
+  .import(collections)
+  .then(() => {
+    console.log('Success')
+  })
+  .catch(err => {
+    console.log('Error', err)
+  })
