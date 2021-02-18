b0918f3a95cb3da622d3f4ea270a497f2f40739e
Denny Petersson <petersson.denny@gmail.com>
2021-02-18 22:20:44 +0100

    adding email validator

--- models/User.js
+++ models/User.js
@@ -1,15 +1,17 @@
 import { Schema, model } from 'mongoose'
+import validator from 'validator'
 
 const userSchema = new Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
+      validate: validator.isEmail
     },
     password: { type: String, required: true, minlength: 8 }
   },
