b1e813bed324eddffbc77a776eaaa81024e7059b
Denny Petersson <petersson.denny@gmail.com>
2021-02-18 22:15:07 +0100

    adding timestamps

--- models/Catch.js
+++ models/Catch.js
@@ -1,16 +1,19 @@
 import { Schema, model } from 'mongoose'
 
-const catchSchema = Schema({
-  username: { type: String, required: true, maxlength: 20 },
-  longitude: { type: Number, required: true },
-  latitude: { type: Number, required: true },
-  locationWater: { type: String, required: true },
-  locationCity: { type: String, required: true },
-  species: { type: String, required: true },
-  weight: { type: Number },
-  length: { type: Number },
-  imageUrl: { type: String },
-  timestamp: { type: Date, required: true, default: Date.now }
-})
+const catchSchema = Schema(
+  {
+    username: { type: String, required: true, maxlength: 20 },
+    longitude: { type: Number, required: true },
+    latitude: { type: Number, required: true },
+    locationWater: { type: String, required: true },
+    locationCity: { type: String, required: true },
+    species: { type: String, required: true },
+    weight: { type: Number },
+    length: { type: Number },
+    imageUrl: { type: String },
+    timestamp: { type: Date, required: true, default: Date.now }
+  },
+  { timestamps: true }
+)
 
 export const Catch = model('catch', catchSchema)
