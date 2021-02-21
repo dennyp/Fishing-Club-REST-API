e79e57645537b3bec15ee0c91b8b9ff543b2b82f
Denny Petersson <petersson.denny@gmail.com>
2021-02-21 22:48:22 +0100

    correcting spelling error

--- data-import/1-catches/catches.cjs
+++ data-import/1-catches/catches.cjs
@@ -1,19 +1,30 @@
 const { getObjectId } = require('mongo-seeding')
 
-const names = ['Nicklas', 'Jakob', 'Lovisa', 'Erik', 'Lotta', 'Isak', 'Lina', 'Linda', 'Amanda', 'Karl']
+const names = [
+  'Nicklas',
+  'Jakob',
+  'Lovisa',
+  'Erik',
+  'Lotta',
+  'Isak',
+  'Lina',
+  'Linda',
+  'Amanda',
+  'Karl'
+]
 const min = 18
 const max = 100
 
-module.exports = names.map(name => ({
+module.exports = names.map((name) => ({
   username: name,
-  longitud: Math.floor(Math.random() * (max - min + 1)) + min,
-  latitud: Math.floor(Math.random() * (max - min + 1)) + min,
+  longitude: Math.floor(Math.random() * (max - min + 1)) + min,
+  latitude: Math.floor(Math.random() * (max - min + 1)) + min,
   locationWater: 'Vättern',
   locationCity: 'Jönköping',
   species: 'Abborre',
   weight: 1.9,
   length: 50,
-  imageUrl: `http://image.com/${name}`,
+  imageUrl: `http://image.com/${name}.jpg`,
   timeOfCatch: Date.now(),
   _id: getObjectId(name)
 }))
