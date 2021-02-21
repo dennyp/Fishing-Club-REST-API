22d62d5c24fbadde5e96814a42f43c2a888ea996
Denny Petersson <petersson.denny@gmail.com>
2021-02-21 12:20:35 +0100

    testing authentication in home router

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,12 +2,13 @@ import express from 'express'
 import createError from 'http-errors'
 import { HomeController } from '../controllers/homeController.js'
 import { router as accountRouter } from '../routes/accountRouter.js'
+import { JWTAuthenticate } from '../utils/auth.js'
 
 export const router = express.Router()
 
 const controller = new HomeController()
 
-router.get('/', controller.index)
+router.get('/', JWTAuthenticate, controller.index)
 router.use('/', accountRouter)
 
 router.use('*', (req, res, next) => next(createError(404)))
