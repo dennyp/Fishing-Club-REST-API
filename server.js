e6e3cac8dd32e061ee9df4fe34e9eb4c9030fe78
Denny Petersson <petersson.denny@gmail.com>
2021-02-23 17:25:25 +0100

    adding logging with morgan

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import helmet from 'helmet'
+import morgan from 'morgan'
 import { connectDatabase } from './config/mongoose.js'
 import { router } from './routes/homeRouter.js'
 
@@ -11,6 +12,8 @@ const main = async () => {
   const app = express()
 
   app.use(helmet())
+
+  app.use(morgan('dev'))
 
   app.use(express.json())
 
