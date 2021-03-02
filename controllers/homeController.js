9bcbf9655ad7347af4e154a98113fb3e5c611ae9
Denny Petersson <petersson.denny@gmail.com>
2021-03-02 15:28:18 +0100

    adding links to root

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,11 @@
 export class HomeController {
-  async index (req, res) {
+  async index(req, res) {
     res.status(200).send({
       _links: {
-        self: { href: '/' }
+        self: { href: `${req.originalUrl}` },
+        catches: { href: `${req.originalUrl}/catches` },
+        register: { href: `${req.originalUrl}/register` },
+        authentication: { href: `${req.originalUrl}/login` }
       }
     })
   }
