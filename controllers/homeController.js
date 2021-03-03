a44af470c3c61e0ab53e7894e04d3f3b83efa306
Denny Petersson <petersson.denny@gmail.com>
2021-03-03 21:43:16 +0100

    removing slashes in path

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,9 +3,9 @@ export class HomeController {
     res.status(200).send({
       _links: {
         self: { href: `${req.originalUrl}` },
-        catches: { href: `${req.originalUrl}/catches` },
-        register: { href: `${req.originalUrl}/register` },
-        authentication: { href: `${req.originalUrl}/login` }
+        catches: { href: `${req.originalUrl}catches` },
+        register: { href: `${req.originalUrl}register` },
+        authentication: { href: `${req.originalUrl}login` }
       }
     })
   }
